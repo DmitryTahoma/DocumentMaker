ddb9302123e1196eb3e4eb0bffe44e44ef39456f
DmitryTahoma <dimon.otrishk@gmail.com>
2022-09-28 11:00:03 +0300

    added Mvvm project

---
+++ Mvvm/Commands/Command.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Mvvm.Commands
+{
+	public class Command : ICommand
+	{
+		bool haveParameter;
+		Action action;
+		Action<object> paramAction;
+		Func<object, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public Command(Action<object> actionCommand, Func<object, bool> canExecute = null)
+		{
+			action = null;
+			paramAction = actionCommand;
+			this.canExecute = canExecute;
+			haveParameter = true;
+		}
+		public Command(Action actionCommand, Func<object, bool> canExecute = null)
+		{
+			action = actionCommand;
+			paramAction = null;
+			this.canExecute = canExecute;
+			haveParameter = false;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute == null || canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			if (haveParameter)
+				paramAction(parameter);
+			else 
+				action();
+		}
+	}
+}
---
+++ Mvvm/Commands/GenericCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Mvvm.Commands
+{
+	public class Command<T> : ICommand where T : class
+	{
+		Action<T> action;
+		Func<T, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public Command(Action<T> actionCommand, Func<T, bool> canExecute = null)
+		{
+			action = actionCommand;
+			this.canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return canExecute == null || (parameter is T tParam && canExecute(tParam));
+		}
+
+		public void Execute(object parameter)
+		{
+			action(parameter as T);
+		}
+	}
+}
