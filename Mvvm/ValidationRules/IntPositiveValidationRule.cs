3e751f8e9c21fb00fff9bed5fbdb0732e7e0a47c
DmitryTahoma <dimon.otrishk@gmail.com>
2022-09-29 16:34:45 +0300

    added validation

---
+++ Mvvm/ValidationRules/IntPositiveValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Mvvm.ValidationRules
+{
+	public class IntPositiveValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			return int.TryParse((value ?? "").ToString(), out int t)
+				? (t > 0 ? ValidationResult.ValidResult : new ValidationResult(false, "Число повинно бути більше 0."))
+				: new ValidationResult(false, "Повинно бути цілим числом.");
+		}
+	}
+}
---
+++ Mvvm/ValidationRules/NotEmptyValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Mvvm.ValidationRules
+{
+	public class NotEmptyValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			return string.IsNullOrWhiteSpace((value ?? "").ToString())
+				? new ValidationResult(false, "Поле має бути заповнене.")
+				: ValidationResult.ValidResult;
+		}
+	}
+}
