cf88c6b3b09a02c3ed1b2c33a41053cbc195315c
DmitryTahoma <dimon.otrishk@gmail.com>
2022-09-30 16:17:54 +0300

    changed HumanEdit view

---
+++ HumanEditorLib/Model/HumanEditModel.cs
@@ -0,0 +1,24 @@
+﻿using Db.Context;
+using Db.Context.HumanPart;
+using System.Collections.Generic;
+
+namespace HumanEditorLib.Model
+{
+	public class HumanEditModel
+	{
+		public HumanEditModel()
+		{
+
+		}
+
+		public IEnumerable<Human> LoadHumans()
+		{
+			List<Human> result = new List<Human>();
+			using(DocumentMakerContext db = new DocumentMakerContext())
+			{
+				result.AddRange(db.Humans);
+			}
+			return result;
+		}
+	}
+}
---
+++ HumanEditorLib/ViewModel/HumanEditViewModel.cs
@@ -0,0 +1,50 @@
+﻿using HumanEditorLib.Model;
+using Mvvm.Commands;
+using System.Windows;
+
+namespace HumanEditorLib.ViewModel
+{
+	public class HumanEditViewModel : DependencyObject
+	{
+		HumanEditModel model = new HumanEditModel();
+		bool loaded = false;
+
+		public HumanEditViewModel()
+		{
+			InitCommands();
+		}
+
+		#region Properties
+
+		public bool ModeSelected
+		{
+			get { return (bool)GetValue(ModeSelectedProperty); }
+			set { SetValue(ModeSelectedProperty, value); }
+		}
+		public static readonly DependencyProperty ModeSelectedProperty = DependencyProperty.Register(nameof(ModeSelected), typeof(bool), typeof(HumanEditViewModel));
+
+		#endregion
+
+		#region Commands
+
+		private void InitCommands()
+		{
+			LoadFromDatabase = new Command(OnLoadFromDatabaseExecute);
+		}
+
+		public Command LoadFromDatabase { get; private set; }
+		public void OnLoadFromDatabaseExecute()
+		{
+			if(!loaded)
+			{
+				loaded = true;
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+	}
+}
