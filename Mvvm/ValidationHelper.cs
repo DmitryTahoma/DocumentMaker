077271c6d938d34e9b8eda4c2e3f279038e5b413
DmitryTahoma <dimon.otrishk@gmail.com>
2022-09-30 15:03:17 +0300

    created ValidationHelper

---
+++ Mvvm/ValidationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Mvvm
+{
+	public static class ValidationHelper
+	{
+		public static bool IsValid(DependencyObject obj)
+		{
+			return !Validation.GetHasError(obj)
+				&& LogicalTreeHelper.GetChildren(obj)
+				   .OfType<DependencyObject>()
+				   .All(IsValid);
+		}
+	}
+}
