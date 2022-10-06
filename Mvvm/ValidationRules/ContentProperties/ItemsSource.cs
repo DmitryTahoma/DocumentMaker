9fad65b36051c1fbf837748f3d23be11262a9dac
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-06 13:50:45 +0300

    added SourceContainsValidationRule

---
+++ Mvvm/ValidationRules/ContentProperties/ItemsSource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mvvm.ValidationRules.ContentProperties
+{
+	public class ItemsSource : DependencyObject
+	{
+		public IEnumerable Value
+		{
+			get { return (IEnumerable)GetValue(ValueProperty); }
+			set { SetValue(ValueProperty, value); }
+		}
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(IEnumerable), typeof(ItemsSource), new PropertyMetadata(null, OnValueChanged));
+
+		private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+		{
+			ItemsSource itemsSource = (ItemsSource)obj;
+			BindingExpressionBase bindingExpressionBase = BindingOperations.GetBindingExpressionBase(itemsSource, BindingToTriggerProperty);
+			bindingExpressionBase?.UpdateSource();
+		}
+
+		public object BindingToTrigger
+		{
+			get { return GetValue(BindingToTriggerProperty); }
+			set { SetValue(BindingToTriggerProperty, value); }
+		}
+		public static readonly DependencyProperty BindingToTriggerProperty = DependencyProperty.Register(nameof(BindingToTrigger), typeof(object), typeof(ItemsSource), new FrameworkPropertyMetadata(default(object), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+	}
+}
---
+++ Mvvm/ValidationRules/SourceContainsValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Linq;
+using System.Windows.Markup;
+using Mvvm.ValidationRules.ContentProperties;
+
+namespace Mvvm.ValidationRules
+{
+	[ContentProperty("ItemsSource")]
+	public class SourceContainsValidationRule : ValidationRule
+	{
+		public ItemsSource ItemsSource { get; set; } = null;
+
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			if (ItemsSource == null || ItemsSource.Value == null)
+				return new ValidationResult(false, "Не заданий SourceContainsValidationRule.ItemsSource");
+
+			return !ItemsSource.Value.OfType<object>().Contains(value)
+				? new ValidationResult(false, "Значення не може бути невизначеним.")
+				: ValidationResult.ValidResult;
+		}
+	}
+}
