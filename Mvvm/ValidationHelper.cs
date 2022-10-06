5d5db85f45c05a0d44102ae97bb216eb8eb3daa6
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-06 11:00:54 +0300

    update ValidationHelper
    added UpdateSource
    added recursivity searching first invalid object

--- Mvvm/ValidationHelper.cs
+++ Mvvm/ValidationHelper.cs
@@ -6,12 +6,44 @@ namespace Mvvm
 {
 	public static class ValidationHelper
 	{
-		public static bool IsValid(DependencyObject obj)
+		public static bool IsValid(DependencyObject obj, bool updateSource = false)
 		{
+			if (updateSource) UpdateSource(obj);
+
 			return !Validation.GetHasError(obj)
 				&& LogicalTreeHelper.GetChildren(obj)
 				   .OfType<DependencyObject>()
-				   .All(IsValid);
+				   .All((x) => IsValid(x, updateSource));
+		}
+
+		public static DependencyObject GetFirstInvalid(DependencyObject obj, bool updateSource = false)
+		{
+			if (updateSource) UpdateSource(obj);
+
+			if (Validation.GetHasError(obj))
+			{
+				return obj;
+			}
+			else
+			{
+				foreach(DependencyObject innerObj in LogicalTreeHelper.GetChildren(obj).OfType<DependencyObject>())
+				{
+					DependencyObject firstInvalid = GetFirstInvalid(innerObj, updateSource);
+					if (firstInvalid != null)
+					{
+						return firstInvalid;
+					}
+				}
+			}
+			return null;
+		}
+
+		private static void UpdateSource(DependencyObject obj)
+		{
+			if(obj is TextBox textBox)
+			{
+				textBox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+			}
 		}
 	}
 }
