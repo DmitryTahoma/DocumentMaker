d6b96de3217e9038dd44f46d1f910970d2fbb67b
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-06 13:49:47 +0300

    added NotNullValidationRule

---
+++ Mvvm/ValidationRules/NotNullValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Mvvm.ValidationRules
+{
+	public class NotNullValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			return value == null
+				? new ValidationResult(false, "Значення не може бути невизначеним.")
+				: ValidationResult.ValidResult;
+		}
+	}
+}
