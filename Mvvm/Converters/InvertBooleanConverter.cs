840d8753dc2865c7c70469f17ea5035a787e3d57
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-14 11:15:48 +0300

    added InvertBooleanConverter

---
+++ Mvvm/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mvvm.Converters
+{
+	[ValueConversion(typeof(bool), typeof(bool))]
+	public class InvertBooleanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value is bool boolValue && boolValue ? false : true;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Convert(value, targetType, parameter, culture);
+		}
+	}
+}
