af7977fa08dbf143de608429de4d998d0833153a
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-20 15:28:31 +0300

    added IntZeroOrPositiveValidationRule

---
+++ Mvvm/ValidationRules/IntZeroOrPositiveValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Mvvm.ValidationRules
+{
+	public class IntZeroOrPositiveValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			return int.TryParse((value ?? "").ToString(), out int t)
+				? (t >= 0 ? ValidationResult.ValidResult : new ValidationResult(false, "Число повинно бути більше або дорівнювати 0."))
+				: new ValidationResult(false, "Повинно бути числом.");
+		}
+	}
+}
