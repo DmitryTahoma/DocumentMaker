3028d5ebbb295cd174217f5cc2e414ad398ab118
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-24 12:29:03 +0300

    added ActGenerator view

---
+++ ActGenerator/Model/HumanDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Dml.Model.Template;
+
+namespace ActGenerator.Model
+{
+	class HumanDataContext
+	{
+		public HumanDataContext(string fullName)
+		{
+			FullName = fullName;
+		}
+
+		public string FullName { get; private set; }
+		public string SumText { get; set; }
+		public DocumentTemplate Template { get; set; }
+	}
+}
---
+++ ActGenerator/ViewModel/ActGeneratorViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ActGenerator.Model;
+using Dml.Model.Template;
+using Mvvm;
+
+namespace ActGenerator.ViewModel
+{
+	class ActGeneratorViewModel
+	{
+		public ActGeneratorViewModel()
+		{
+			ProjectsList = new ObservableRangeCollection<string>
+			{
+				"Lost Lands 8",
+				"Tricky doors",
+				"Lost Lands Stories",
+				"Legendary Tales 3",
+			};
+			HumanList = new ObservableRangeCollection<HumanDataContext>
+			{
+				new HumanDataContext("Алєйникова Марина"),
+				new HumanDataContext("Баєв Олександр"),
+				new HumanDataContext("Байдуж Максим"),
+				new HumanDataContext("Байдуж Марина"),
+			};
+		}
+
+		#region Properties
+
+		public ObservableRangeCollection<string> ProjectsList { get; private set; } = new ObservableRangeCollection<string>();
+
+		public ObservableRangeCollection<HumanDataContext> HumanList { get; private set; } = new ObservableRangeCollection<HumanDataContext>();
+
+		public ObservableRangeCollection<DocumentTemplate> DocumentTemplates { get; private set; } = new ObservableRangeCollection<DocumentTemplate> 
+		{
+			new DocumentTemplate("Скриптувальник", DocumentTemplateType.Scripter),
+			new DocumentTemplate("Технічний дизайнер", DocumentTemplateType.Cutter),
+			new DocumentTemplate("Художник", DocumentTemplateType.Painter),
+			new DocumentTemplate("Моделлер", DocumentTemplateType.Modeller),
+		};
+
+		#endregion
+	}
+}
