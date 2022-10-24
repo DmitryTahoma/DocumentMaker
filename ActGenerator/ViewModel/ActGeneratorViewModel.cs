debb0f600c82248733376c7562ac1830be64cd99
DmitryTahoma <dimon.otrishk@gmail.com>
2022-10-24 15:49:30 +0300

    added ListSelector to the ActGenerator

--- ActGenerator/ViewModel/ActGeneratorViewModel.cs
+++ ActGenerator/ViewModel/ActGeneratorViewModel.cs
@@ -1,6 +1,12 @@
 ﻿using ActGenerator.Model;
+using ActGenerator.View.Dialogs;
+using ActGenerator.ViewModel.Dialogs;
+using Db.Context.BackPart;
 using Dml.Model.Template;
+using MaterialDesignThemes.Wpf;
 using Mvvm;
+using Mvvm.Commands;
+using System.Collections.Generic;
 
 namespace ActGenerator.ViewModel
 {
@@ -8,6 +14,8 @@ namespace ActGenerator.ViewModel
 	{
 		public ActGeneratorViewModel()
 		{
+			InitCommands();
+
 			ProjectsList = new ObservableRangeCollection<string>
 			{
 				"Lost Lands 8",
@@ -26,6 +34,8 @@ namespace ActGenerator.ViewModel
 
 		#region Properties
 
+		public string DialogHostId { get; } = "ActGeneratorDialogHost";
+
 		public ObservableRangeCollection<string> ProjectsList { get; private set; } = new ObservableRangeCollection<string>();
 
 		public ObservableRangeCollection<HumanDataContext> HumanList { get; private set; } = new ObservableRangeCollection<HumanDataContext>();
@@ -37,6 +47,41 @@ namespace ActGenerator.ViewModel
 			new DocumentTemplate("Художник", DocumentTemplateType.Painter),
 			new DocumentTemplate("Моделлер", DocumentTemplateType.Modeller),
 		};
+
+		#endregion
+
+		#region Commands
+
+		private void InitCommands()
+		{
+			AddProjectCommand = new Command(OnAddProjectCommandExecute);
+			CloseOpenedDialog = new Command(OnCloseOpenedDialogExecute);
+		}
+
+		public Command AddProjectCommand { get; private set; }
+		private async void OnAddProjectCommandExecute()
+		{
+			ListSelector listSelector = new ListSelector();
+			ListSelectorViewModel listSelectorViewModel = (ListSelectorViewModel)listSelector.DataContext;
+			listSelectorViewModel.ItemsDisplayMemberPath = nameof(Project.Name);
+			listSelectorViewModel.SetItems(new List<Project> 
+			{
+				new Project { Name="Project1" }, 
+				new Project { Name="Project2" }, 
+				new Project { Name="Project3" }, 
+				new Project { Name="Project4" }, 
+			});
+			await DialogHost.Show(listSelector, DialogHostId);
+		}
+
+		public Command CloseOpenedDialog { get; private set; }
+		private void OnCloseOpenedDialogExecute()
+		{
+			if(DialogHost.IsDialogOpen(DialogHostId))
+			{
+				DialogHost.Close(DialogHostId);
+			}
+		}
 
 		#endregion
 	}
---
+++ ActGenerator/ViewModel/Dialogs/ListSelectorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using MaterialDesignThemes.Wpf;
+using Mvvm;
+using Mvvm.Commands;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows;
+
+namespace ActGenerator.ViewModel.Dialogs
+{
+	class ListSelectorViewModel : DependencyObject
+	{
+		private ObservableRangeCollection<object> items = new ObservableRangeCollection<object>();
+
+		public ListSelectorViewModel()
+		{
+			InitCommands();
+		}
+
+		#region Properties
+
+		public INotifyCollectionChanged Items => items;
+
+		public string ItemsDisplayMemberPath
+		{
+			get { return (string)GetValue(ItemsDisplayMemberPathProperty); }
+			set { SetValue(ItemsDisplayMemberPathProperty, value); }
+		}
+		public static readonly DependencyProperty ItemsDisplayMemberPathProperty = DependencyProperty.Register(nameof(ItemsDisplayMemberPath), typeof(string), typeof(ListSelectorViewModel));
+
+		public IList SelectedItems { get; private set; } = null;
+
+		#endregion
+
+		#region Commands
+
+		private void InitCommands()
+		{
+			AddCommand = new Command<IList>(OnAddCommandExecute);
+		}
+
+		public Command<IList> AddCommand { get; private set; }
+		private void OnAddCommandExecute(IList selectedItems)
+		{
+			DialogHost.CloseDialogCommand.Execute(null, null);
+			SelectedItems = selectedItems;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void SetItems(IEnumerable items)
+		{
+			this.items.SuppressingNotifications = true;
+			this.items.Clear();
+			this.items.SuppressingNotifications = false;
+			this.items.AddRange(items.Cast<object>());
+		}
+
+		#endregion
+	}
+}
