a2d4ed0e3f8b954197286f96c792970470fb8c02
DmitryTahoma <39384159+DmitryTahoma@users.noreply.github.com>
2022-10-30 18:59:32 +0300

    added ContentPropertyBase

---
+++ Mvvm/ValidationRules/ContentProperties/ContentPropertyBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Mvvm.ValidationRules.ContentProperties
+{
+	public abstract class ContentPropertyBase<T> : DependencyObject
+	{
+		public T Value
+		{
+			get { return (T)GetValue(ValueProperty); }
+			set { SetValue(ValueProperty, value); }
+		}
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(T), typeof(ContentPropertyBase<T>), new PropertyMetadata(null, OnValueChanged));
+
+		private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+		{
+			BindingExpressionBase bindingExpressionBase = BindingOperations.GetBindingExpressionBase(obj, BindingToTriggerProperty);
+			bindingExpressionBase?.UpdateSource();
+		}
+
+		public object BindingToTrigger
+		{
+			get { return GetValue(BindingToTriggerProperty); }
+			set { SetValue(BindingToTriggerProperty, value); }
+		}
+		public static readonly DependencyProperty BindingToTriggerProperty = DependencyProperty.Register(nameof(BindingToTrigger), typeof(object), typeof(ContentPropertyBase<T>), new FrameworkPropertyMetadata(default(object), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+	}
+}
