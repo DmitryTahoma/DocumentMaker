1ba0bda0e22dd4a8266c0560859f64eb8b7e3b3b
DmitryTahoma <dimon.otrishk@gmail.com>
2022-11-16 11:28:21 +0200

    loading removed elements in ProjectRestore

---
+++ ProjectEditorLib/Model/ProjectRestoreModel.cs
@@ -0,0 +1,63 @@
+﻿using ProjectsDb;
+using ProjectsDb.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEditorLib.Model
+{
+	public class ProjectRestoreModel
+	{
+		ProjectsDbContext db = null;
+
+		~ProjectRestoreModel()
+		{
+			ReleaseContext();
+		}
+
+		public async Task ConnectDB()
+		{
+			await Task.Run(() => { db = new ProjectsDbContext(); });
+		}
+
+		public async Task DisconnectDB()
+		{
+			await Task.Run(ReleaseContext);
+		}
+
+		private void ReleaseContext()
+		{
+			if (db != null)
+			{
+				db.Dispose();
+				db = null;
+			}
+		}
+
+		public Task<List<Project>> LoadRemovedNodes()
+		{
+			return Task.Run(() =>
+			{
+				List<Project> projects =
+					db.Projects
+						.Where(x =>
+							db.Backs
+								.FirstOrDefault(y => y.DeletionDate != null && y.ProjectId == x.Id) != null)
+						.ToList();
+
+				foreach (Project project in projects)
+				{
+					project.Backs =
+						db.Backs
+							.Where(x => x.DeletionDate != null && x.ProjectId == project.Id)
+							.ToList();
+					project.Backs
+						.ForEach(x => x.BackType = db.BackTypes
+							.FirstOrDefault(y => y.Id == x.BackTypeId));
+				}
+
+				return projects;
+			});
+		}
+	}
+}
---
+++ ProjectEditorLib/ViewModel/ProjectRestoreViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Mvvm;
+using Mvvm.Commands;
+using ProjectEditorLib.Model;
+using ProjectEditorLib.View;
+using ProjectsDb.Context;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ProjectEditorLib.ViewModel
+{
+	public class ProjectRestoreViewModel
+	{
+		ProjectRestoreModel model = new ProjectRestoreModel();
+
+		public ProjectRestoreViewModel()
+		{
+			InitCommands();
+		}
+
+		#region Properties
+
+		public ObservableRangeCollection<TreeViewItem> TreeItems { get; private set; } = new ObservableRangeCollection<TreeViewItem>();
+
+		#endregion
+
+		#region Commands
+
+		private void InitCommands()
+		{
+			LoadFromDatabase = new Command(OnLoadFromDatabaseExecute);
+		}
+
+		public Command LoadFromDatabase { get; private set; }
+		private async void OnLoadFromDatabaseExecute()
+		{
+			await model.ConnectDB();
+			List<Project> projects = await model.LoadRemovedNodes();
+			await model.DisconnectDB();
+
+			TreeItems.Clear();
+			foreach(Project project in projects)
+			{
+				TreeViewItem projectTree = CreateTreeViewItem(ProjectNodeType.Project, project, null);
+				TreeItems.Add(projectTree);
+
+				foreach(Back back in project.Backs)
+				{
+					projectTree.Items.Add(CreateNodeByType(back, projectTree, out _));
+				}
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		private TreeViewItem CreateNodeByType(Back back, TreeViewItem parrent, out ProjectNodeType nodeType)
+		{
+			nodeType = (ProjectNodeType)Enum.Parse(typeof(ProjectNodeType), back.BackType.Name);
+			return CreateTreeViewItem(nodeType, back, parrent);
+		}
+
+		private TreeViewItem CreateTreeViewItem(ProjectNodeType nodeType, IDbObject context, TreeViewItem parrent)
+		{
+			TreeItemHeader nodeHeader = new TreeItemHeader();
+			TreeItemHeaderViewModel nodeHeaderViewModel = (TreeItemHeaderViewModel)nodeHeader.DataContext;
+			nodeHeaderViewModel.SetModel(new ProjectNode(nodeType, context));
+			TreeViewItem treeViewItem = new TreeViewItem { Header = nodeHeader };
+			//nodeHeaderViewModel.AddCommand = ConvertAddingCommand(treeViewItem, AddTreeViewItemCommand);
+			//nodeHeaderViewModel.RemoveCommand = ConvertRemovingCommand(treeViewItem, RemoveTreeViewItemCommand);
+
+			Binding contextMenuBinding = new Binding(nameof(TreeItemHeaderViewModel.ContextMenuProperty))
+			{
+				Source = nodeHeaderViewModel,
+				Path = new PropertyPath(nameof(nodeHeaderViewModel.ContextMenu)),
+				Mode = BindingMode.OneWay,
+				UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+			};
+			treeViewItem.SetBinding(FrameworkElement.ContextMenuProperty, contextMenuBinding);
+
+			if (parrent != null)
+			{
+				TreeItemHeader parrentHeader = (TreeItemHeader)parrent.Header;
+				TreeItemHeaderViewModel parrentHeaderViewModel = (TreeItemHeaderViewModel)parrentHeader.DataContext;
+				parrentHeaderViewModel.AddChild(nodeHeaderViewModel);
+			}
+
+			return treeViewItem;
+		}
+
+		#endregion
+	}
+}
