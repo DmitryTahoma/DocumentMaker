ae611e182849109758ed332f6852835618f382cc
DmitryTahoma <dimon.otrishk@gmail.com>
2022-11-22 12:04:24 +0200

    added CryptedConnectionString

---
+++ Security/CryptedConnectionString.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+
+namespace DocumentMaker.Security
+{
+	public class CryptedConnectionString
+	{
+		readonly Crypter crypter = new Crypter();
+		readonly string cryptKey = Assembly.GetExecutingAssembly().Location;
+
+		public string ConnectionString { get; set; }
+		public bool IsCrypted { get; set; }
+
+		public void Crypt()
+		{
+			if (!IsCrypted)
+			{
+				ConnectionString = crypter.Protect(ConnectionString, cryptKey, DataProtectionScope.CurrentUser);
+				IsCrypted = true;
+			}
+		}
+
+		public string GetDecryptedConnectionString()
+		{
+			return IsCrypted ? crypter.Unprotect(ConnectionString, cryptKey, DataProtectionScope.CurrentUser) : ConnectionString;
+		}
+
+		public bool TryGetDecryptedConnectionString(out string decryptedConnectionString)
+		{
+			try
+			{
+				decryptedConnectionString = GetDecryptedConnectionString();
+				return true;
+			}
+			catch
+			{
+				decryptedConnectionString = null;
+				return false;
+			}
+		}
+	}
+}
---
+++ Security/Crypter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DocumentMaker.Security
+{
+	public class Crypter
+	{
+		public string Protect(string stringToEncrypt, string optionalEntropy, DataProtectionScope scope)
+		{
+			return Convert.ToBase64String(
+				ProtectedData.Protect(
+					Encoding.UTF8.GetBytes(stringToEncrypt),
+					optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null,
+					scope));
+		}
+
+		public string Unprotect(string encryptedString, string optionalEntropy, DataProtectionScope scope)
+		{
+			return Encoding.UTF8.GetString(
+				ProtectedData.Unprotect(
+					Convert.FromBase64String(encryptedString),
+					optionalEntropy != null ? Encoding.UTF8.GetBytes(optionalEntropy) : null,
+					scope));
+		}
+	}
+}
