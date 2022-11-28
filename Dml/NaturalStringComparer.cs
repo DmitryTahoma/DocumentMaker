499f15a6837891aec65d06a20ae7c2b8d66d51d2
DmitryTahoma <dimon.otrishk@gmail.com>
2022-11-28 14:51:35 +0200

    added NaturalStringComparer

---
+++ Dml/NaturalStringComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Dml
+{
+	public class NaturalStringComparer : IComparer, IComparer<string>
+	{
+		[DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+		public static extern int StrCmpLogicalW(string a, string b);
+
+		public int Compare(object a, object b)
+		{
+			return Compare(a?.ToString(), b?.ToString());
+		}
+
+		public int Compare(string a, string b)
+		{
+			return StrCmpLogicalW(a, b);
+		}
+	}
+}
