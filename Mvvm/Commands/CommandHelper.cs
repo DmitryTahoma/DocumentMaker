2f826f0f123629b29105b76566b22b8c48982b4c
DmitryTahoma <dimon.otrishk@gmail.com>
2022-12-01 12:36:15 +0200

    added CommandHelper

---
+++ Mvvm/Commands/CommandHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace Mvvm.Commands
+{
+	public static class CommandHelper
+	{
+		public static bool UpdateAllCanExecute(object _)
+		{
+			CommandManager.InvalidateRequerySuggested();
+			return true;
+		}
+	}
+}
