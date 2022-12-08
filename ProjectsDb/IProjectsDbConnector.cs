d3a83e09f95d3881b623b24df7d3c735067d834d
DmitryTahoma <dimon.otrishk@gmail.com>
2022-12-08 10:14:17 +0200

    added ProjectsDbConnector

---
+++ ProjectsDb/IProjectsDbConnector.cs
@@ -0,0 +1,22 @@
+﻿using DocumentMaker.Security;
+using ProjectsDb.Context;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectsDb
+{
+	public interface IProjectsDbConnector
+	{
+		bool CryptedConnectionStringSetted { get; }
+
+		void ConnectDb();
+		Task ConnectDbAsync();
+		void DisconnectDb();
+		Task DisconnectDbAsync();
+		IEnumerable<T> GetTable<T>() where T : class, IDbObject;
+		Task<IEnumerable<T>> GetTableAsync<T>() where T : class, IDbObject;
+		void SyncCollection<T>(ICollection<T> collection) where T : class, IDbObject;
+		Task SyncCollectionAsync<T>(ICollection<T> collection) where T : class, IDbObject;
+		void SetConnectionString(CryptedConnectionString cryptedConnectionString);
+	}
+}
---
+++ ProjectsDb/ProjectsDbConnector.cs
@@ -0,0 +1,75 @@
+﻿using DocumentMaker.Security;
+using ProjectsDb.Context;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectsDb
+{
+	public class ProjectsDbConnector : IProjectsDbConnector
+	{
+		protected ProjectsDbContext db = null;
+		private CryptedConnectionString cryptedConnectionString = null;
+
+		~ProjectsDbConnector()
+		{
+			ReleaseContext();
+		}
+
+		public bool CryptedConnectionStringSetted { get; private set; }
+
+		private void ReleaseContext()
+		{
+			if (db != null)
+			{
+				db.Dispose();
+				db = null;
+			}
+		}
+
+		public void ConnectDb()
+		{
+			db = new ProjectsDbContext(cryptedConnectionString.GetDecryptedConnectionString());
+		}
+
+		public Task ConnectDbAsync()
+		{
+			return Task.Run(ConnectDb);
+		}
+
+		public void DisconnectDb()
+		{
+			ReleaseContext();
+		}
+
+		public Task DisconnectDbAsync()
+		{
+			return Task.Run(DisconnectDb);
+		}
+
+		public IEnumerable<T> GetTable<T>() where T : class, IDbObject
+		{
+			return db.GetTable<T>();
+		}
+
+		public Task<IEnumerable<T>> GetTableAsync<T>() where T : class, IDbObject
+		{
+			return db.GetTableAsync<T>();
+		}
+
+		public void SyncCollection<T>(ICollection<T> collection) where T : class, IDbObject
+		{
+			db.SyncCollection(collection);
+		}
+
+		public Task SyncCollectionAsync<T>(ICollection<T> collection) where T : class, IDbObject
+		{
+			return db.SyncCollectionAsync(collection);
+		}
+
+		public void SetConnectionString(CryptedConnectionString cryptedConnectionString)
+		{
+			this.cryptedConnectionString = cryptedConnectionString;
+			CryptedConnectionStringSetted = true;
+		}
+	}
+}
