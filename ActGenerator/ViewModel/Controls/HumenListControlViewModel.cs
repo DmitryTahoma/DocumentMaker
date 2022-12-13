7562a5cc1b937e3bf46da34b1ae39ceedf0dd462
DmitryTahoma <dimon.otrishk@gmail.com>
2022-12-13 12:08:21 +0200

    created AddHumanDialog

--- ActGenerator/ViewModel/Controls/HumenListControlViewModel.cs
+++ ActGenerator/ViewModel/Controls/HumenListControlViewModel.cs
@@ -1,10 +1,21 @@
-﻿using Dml;
+﻿using ActGenerator.View.Dialogs;
+using ActGenerator.ViewModel.Interfaces;
+using Dml;
 using Dml.Model.Template;
+using MaterialDesignThemes.Wpf;
+using Mvvm.Commands;
 
 namespace ActGenerator.ViewModel.Controls
 {
-	public class HumenListControlViewModel
+	public class HumenListControlViewModel : IContainDialogHostId
 	{
+		AddHumanDialog addHumanDialog = new AddHumanDialog();
+
+		public HumenListControlViewModel()
+		{
+			InitCommands();
+		}
+
 		#region Properties
 
 		public ObservableRangeCollection<DocumentTemplate> DocumentTemplatesList => new ObservableRangeCollection<DocumentTemplate>
@@ -20,6 +31,23 @@ namespace ActGenerator.ViewModel.Controls
 				new DocumentTemplate("Перекладач", DocumentTemplateType.Translator),
 				new DocumentTemplate("Підтримка", DocumentTemplateType.Support),
 			};
+
+		public string DialogHostId { get; set; } = null;
+
+		#endregion
+
+		#region Commands
+
+		private void InitCommands()
+		{
+			AddHumanCommand = new Command(OnAddHumanCommandExecute);
+		}
+
+		public Command AddHumanCommand { get; private set; }
+		private async void OnAddHumanCommandExecute()
+		{
+			await DialogHost.Show(addHumanDialog, DialogHostId);
+		}
 
 		#endregion
 	}
