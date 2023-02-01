180d69a6a74963985a3092b89760ae570f49578f
DmitryTahoma <dimon.otrishk@gmail.com>
2023-02-01 11:39:42 +0200

    created ProjectNameDialogItem

---
+++ ActGenerator/ViewModel/Dialogs/Controls/ProjectNameDialogItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProjectsDb.Context;
+
+namespace ActGenerator.ViewModel.Dialogs.Controls
+{
+	class ProjectNameDialogItemViewModel
+	{
+		#region Properties
+
+		public Project Project { get; set; } = null;
+
+		public AlternativeProjectName AlternativeProjectName { get; set; } = null;
+
+		#endregion
+	}
+}
